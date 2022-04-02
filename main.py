3516f9354666726320ab685f44f3c3c792ffdfcc
金城理奈 <dontako0126@gmail.com>
2022-04-02 10:42:49 +0900

    env読み込み追加

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from dotenv import load_dotenv
-load_dotenv()
+import sys
+extDataDir = os.getcwd()
+if getattr(sys, 'frozen', False):
+    extDataDir = sys._MEIPASS
+load_dotenv(dotenv_path=os.path.join(extDataDir, '.env'))
+
+
+#load_dotenv(dotenv_path=".env")
 import os
 word = os.getenv('word')
-print(word)
+from datetime import datetime
+
+s = input("お名前は？")
+y = int(input(f"こんにちは,{s}さん。歳は？"))
+
+py = datetime.now().year-1991
+
+if y== py:
+    print("同い年")
+    print(word)
+elif y < py:
+    print("若い!")
+    print(word)
+else:
+    print("年上!")
+    print(word)
+
+input("何かキーを押すと終了")
